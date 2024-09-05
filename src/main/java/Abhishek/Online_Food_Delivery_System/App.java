b3f777a84675a3ba979b99d696aa01e2024b7f6c
Abhishek <abhishek123@gmail.com>
2024-09-05 12:45:05 +0530

    initial commit

---
+++ src/main/java/Abhishek/Online_Food_Delivery_System/App.java
@@ -0,0 +1,13 @@
+package Abhishek.Online_Food_Delivery_System;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
